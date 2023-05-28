314d003d171fb35fb2cd22f44b924116adac37cd
Turyahabwa Mark <profturyahabwa@gmail.com>
2023-05-28 17:45:13 +0300

    simple additions

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ const App = () => {
       <Route path='/' element={<Navigation />}>
         <Route index element={<Home/>} />
         <Route path='shop' element={<Shop />} />
+        <Route path='sign-in' element={<SignIn />}/>
       </Route>
     </Routes>
   )
--- src/routes/NavBar/navigation-component.jsx
+++ src/routes/NavBar/navigation-component.jsx
@@ -1,22 +1,22 @@
-import {Fragment} from 'react'
+import { Fragment } from 'react'
 import { Outlet, Link } from "react-router-dom";
 
-import {ReactComponent as EliteLogo} from '../../assets/elite.svg';
+import { ReactComponent as EliteLogo } from '../../assets/elite.svg';
 import './nav-styles.scss'
 
 const Navigation = () => {
-    return(
+    return (
         <Fragment>
-        <nav className='navigation'>
-            <Link className="logo-container" to='/'>
-                <EliteLogo className='logo' />
-            </Link>
-            <div className="nav-links-container">
-            <Link className="nav-link" to='/shop'>SHOP</Link>
-            <Link className='nav-link' to='sign-in'>SIGN IN</Link>
-            </div>
-        </nav>
-        <Outlet />
+            <nav className='navigation'>
+                <Link className="logo-container" to='/'>
+                    <EliteLogo className='logo' />
+                </Link>
+                <div className="nav-links-container">
+                    <Link className="nav-link" to='/shop'>SHOP</Link>
+                    <Link className='nav-link' to='/sign-in'>SIGN IN</Link>
+                </div>
+            </nav>
+            <Outlet />
         </Fragment>
     )
 }
